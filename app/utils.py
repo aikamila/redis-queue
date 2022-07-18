c4fddfaffa9d1f1a0f676afc0f6ef6c27a453382
Kamila Luczaj <kamilcia1417@wp.pl>
2022-07-18 14:05:43 +0200

    Implemented the algorithm

--- app/utils.py
+++ app/utils.py
@@ -5,10 +5,33 @@ import heapq
 import functools
 
 
+@functools.total_ordering
+class Element:
+    def __init__(self, string, counter):
+        self.string = string
+        self.counter = counter
+
+    def __lt__(self, other):
+        if self.counter == other.counter:
+            return self.string > other.string
+        return self.counter < other.counter
+
+    def __eq__(self, other):
+        return self.counter == other.counter and self.string == other.string
+
+
 def find_3_most_popular(url: str):
     # simulating long response time
     time.sleep(20)
     resp = requests.get(url)
     c = Counter(resp.text.split())
-
-    return ['hi', 'nice', 'day']
+    freqs = []
+    heapq.heapify(freqs)
+    for key, value in c.items():
+        heapq.heappush(freqs, (Element(key, value), key))
+        if len(freqs) > 3:
+            heapq.heappop(freqs)
+    res = []
+    for _ in range(3):
+        res.append(heapq.heappop(freqs)[1])
+    return res[::-1]
