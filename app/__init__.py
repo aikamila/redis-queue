9454a165b62a8c35d33d467c6905b366ea911b0a
Kamila Luczaj <kamilcia1417@wp.pl>
2022-07-18 13:23:58 +0200

    Basic functionalities - dummy word counting

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 import os
 
 from rq import Queue
+from rq.job import Job
 from .worker import conn
+from .utils import find_3_most_popular
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 
-q = Queue('high', connection=conn)
+q1 = Queue('high', connection=conn)
+q2 = Queue('low', connection=conn)
 
 
 def create_app():
@@ -17,5 +20,24 @@ def create_app():
 
     @app.route('/', methods=('GET',))
     def index():
+        url = request.args.get('url', None)
+        if url:
+            result = q1.enqueue(find_3_most_popular, url, result_ttl=86400)
+            return redirect(url_for('process', id=result.id))
         return render_template('index.html')
+
+    @app.route('/processing/<id>', methods=('GET',))
+    def process(id):
+        return render_template('process.html', id=id)
+
+    @app.route('/check/<id>', methods=('GET',))
+    def check(id):
+        if Job.fetch(id=id, connection=conn) and \
+                Job.fetch(id=id, connection=conn).result:
+            return jsonify({'ready': True})
+        return jsonify({'ready': False})
+
+    @app.route('/result/<id>', methods=('GET',))
+    def result(id):
+        return render_template('result.html', words=Job.fetch(id=id, connection=conn).result)
     return app
---
+++ app/utils.py
@@ -0,0 +1,6 @@
+import time
+
+
+def find_3_most_popular(url: str):
+    time.sleep(50)
+    return ['hi', 'nice', 'day']
